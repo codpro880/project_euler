a2fff608539a83d1474d1a5e6b9589c60902e1ba
Matt Messersmith <messersmith.matt@gmail.com>
2017-05-26 22:49:08 -0400

    Project euler solution for problem 2

---
+++ problem_2.py
@@ -0,0 +1,20 @@
+def fib():
+    yield 1
+    yield 2
+    first = 1
+    second = 2
+    while True:
+        yield first + second
+        first, second = second, first + second
+
+def sum_even_fib_less_than_4mil():
+    fib_gen = fib()
+    cur_fib = next(fib_gen)
+    result = 0
+    while cur_fib < 4000000:
+        if cur_fib % 2 == 0:
+            result += cur_fib
+        cur_fib = next(fib_gen)
+    return result
+
+print(sum_even_fib_less_than_4mil())
