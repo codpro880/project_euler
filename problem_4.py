89e9e36da891286f7a00e629654f8bfea341831e
Matt Messersmith <messersmith.matt@gmail.com>
2017-05-26 23:59:05 -0400

    Solution to problem 4

---
+++ problem_4.py
@@ -0,0 +1,11 @@
+""" Find the largest palindrome made from the product of two three digit numbers """
+def is_palindrome(x):
+    return str(x) == str(x)[::-1]
+
+palindromes = []
+for x in range(999)[:50:-1]:
+    for y in range(999)[:50:-1]:
+        if is_palindrome(x * y):
+            palindromes.append(x * y)
+
+print(max(palindromes))
